a7bf89f9623d27c697e71b38e1cf2d8dfd8bcbc7
Arturs Radionovs <artursradionovs@Arturss-Air.broadband>
2023-10-22 10:16:16 +0100

    first-commit

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,74 +1,114 @@
-use hyper::{Body, Request, Response, Server, StatusCode};
+#![allow(dead_code)]
+
+use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use std::net::SocketAddr;
-
+use std::net::IpAddr;
+use std::time::SystemTime;
+use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
+use jsonwebtoken::{Header, Algorithm, EncodingKey};
+use chrono::{Utc, Duration};
 
 #[derive(Debug)]
 
 struct RateLimiter {
-    visitors: Arc<Mutex<HashMap<SocketAddr, u32>>>,
-    }
-
+    visitors: Arc<Mutex<HashMap<IpAddr, (u32, SystemTime)>>>,
+}
 
 impl RateLimiter {
-    fn new() -> Self { 
+    fn new() -> Self {
         RateLimiter {
-        visitors: Arc::new(Mutex::new(HashMap::new())), 
-    }}
-    fn allow(&self, addr: SocketAddr) -> bool {
-    let mut visitors = self.visitors.lock().unwrap(); 
-    let counter = visitors.entry(addr).or_insert(0); 
-    if *counter >= 5 {
-        false
-    } else {
-        *counter += 1;
-        true
-    } 
+            visitors: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+
+    fn allow(&self, addr: IpAddr) -> bool {
+
+        // grabbing the current time
+        let request_time = SystemTime::now();
+        // locking the hashmap preventing multiple threads from accessing it at the same time
+        let mut visitors = self.visitors.lock().unwrap();
+        // checking if the ip address is in the hashmap, and if it is there, we are returning the value
+        let counter = visitors.entry(addr).or_insert((0, request_time));
+    
+        // Check if 30 seconds have passed since the first request
+        if request_time.duration_since(counter.1).unwrap().as_secs() >= 30 {
+            // Reset the request count and timestamp
+            counter.0 = 0;
+            counter.1 = request_time;
+        }
+        
+        if counter.0 >= 50 {
+            false
+        } else {
+            counter.0 += 1;
+            true
+        }
+    }
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GatewayAuthClaims {
+    iss: String, // Issuer
+    gateway_access: bool, // Custom claim for gateway access
+    exp: i64, // Expiration Time
+    iat: i64, // Issued At
+}
+
+impl GatewayAuthClaims {
+    pub fn new(issuer: String, gateway_access: bool) -> Self {
+        let iat = Utc::now();
+        let exp = iat + Duration::hours(72);
+    
+        Self {
+            iss: issuer,
+            gateway_access,
+            exp: exp.timestamp(),
+            iat: iat.timestamp(),
+        }
+    }
 }
 
 
-async fn handle_request(req: Request<Body>, rate_limiter: Arc<RateLimiter>, addr: SocketAddr) -> Result<Response<Body>, hyper::Error> {
+async fn handle_request(req: Request<Body>, rate_limiter: Arc<RateLimiter>, addr: IpAddr) -> Result<Response<Body>, hyper::Error> {
+    let issuer = "gateway".to_string();
+    let gateway_access = true;
+    let jwt_secret: String = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set");
+
+    let jwt_token = jsonwebtoken::encode(&Header::new(Algorithm::HS256), &GatewayAuthClaims::new(issuer, gateway_access, ),&EncodingKey::from_secret(jwt_secret.as_bytes())).unwrap();
     match req.uri().path() {
-        "/service1" => {
-            let mut response = Response::new(Body::from("Hello from service1"));
-            *response.status_mut() = hyper::StatusCode::CREATED;
-            Ok(response)
-        },
-        "/service2" => {
-            let mut response = Response::new(Body::from("Hello from service2"));
-            *response.status_mut() = hyper::StatusCode::CREATED;
-            Ok(response)
-        },
-        "/service3" => {
-            println!("{}" , addr);
-            println!("{}" , rate_limiter.allow(addr));
-
+        "/service" => {
+           
             if !rate_limiter.allow(addr) {
-                return Ok(Response::new(Body::from("Rate limit exceeded for service3")))
+                return Ok(Response::new(Body::from("Rate limit exceeded for service")))
             } 
-
             let client = hyper::Client::new();
-            let forwarded_req = Request::builder().method(req.method()).uri("http://userservice:3000/api/v1/users/allusers").body(req.into_body())
-            .unwrap();
+            let forwarded_req = Request::builder()
+                .method(req.method())
+                .uri("http://localhost:3000")
+                .header("Authorization", format!("Bearer {}", jwt_token))
+                .body(req.into_body())
+                .unwrap();
             let mut resp: Response<Body> = client.request(forwarded_req).await?;
             *resp.status_mut() = hyper::StatusCode::CREATED;
             Ok(resp)
         },
+        
         _ => Ok(Response::new(Body::from("Not Found"))), 
     }
 }
 
 
+
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let rate_limiter = Arc::new(RateLimiter::new());
-    
     let make_svc = make_service_fn(|conn: &hyper::server::conn::AddrStream| {
         let rate_limiter = rate_limiter.clone();
-        let address = conn.remote_addr();
+        let address = conn.remote_addr().ip();
         let service = service_fn(move |req| handle_request(req, rate_limiter.clone(), address));
         async move { Ok::<_, hyper::Error>(service) }
     });
--- userservice/src/index.ts
+++ userservice/src/index.ts
@@ -1,6 +1,40 @@
-import { Elysia } from "elysia";
+import { Elysia, NotFoundError } from "elysia";
+import { jwt } from '@elysiajs/jwt'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+
+const app = new Elysia()
+.use(
+  jwt({
+    secret: Bun.env.JWT_SECRET as string,
+  })
+)
+.get('/', async ({ jwt, request, set }) => {
+  const headers = request.headers;
+   const authorizationHeader = headers.get('authorization');
+   if (authorizationHeader) {
+    const token = authorizationHeader.split(' ')[1];
+
+    const payload = await jwt.verify(token);
+    if (typeof payload === 'object' && payload !== null) {
+      const isExpired = payload.exp
+        ? Date.now() > payload.exp * 1000
+        : false;
+        console.log(isExpired)
+      if ((payload.iss === 'gateway' ) && ((Boolean(payload.gateway_access)) === true) && !isExpired) {
+        set.status = 200
+        return 'Hello world'
+      }
+    }
+    // set.status = 404
+    // return 'Not found'
+    //NotFoundError 
+    throw new NotFoundError('Not found')
+  } 
+
+  throw new NotFoundError('Not found')
+})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
